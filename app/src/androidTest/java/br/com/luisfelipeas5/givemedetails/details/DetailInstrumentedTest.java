62acdca17636b87c7e50a6d2c33f3f9a85778d29
luisfelipeas5 <luisfelipeas5@gmail.com>
2017-07-30 12:55:58 -0300

    - Dagger component mocked implemented - implementing others tests to detail screen

--- app/src/androidTest/java/br/com/luisfelipeas5/givemedetails/details/DetailInstrumentedTest.java
+++ app/src/androidTest/java/br/com/luisfelipeas5/givemedetails/details/DetailInstrumentedTest.java
@@ -2,7 +2,10 @@ package br.com.luisfelipeas5.givemedetails.details;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -10,6 +13,10 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import br.com.luisfelipeas5.givemedetails.R;
 import br.com.luisfelipeas5.givemedetails.model.model.Movie;
@@ -19,6 +26,7 @@ import br.com.luisfelipeas5.givemedetails.view.di.components.BaseComponent;
 import br.com.luisfelipeas5.givemedetails.view.di.components.DaggerAppTestComponent;
 import br.com.luisfelipeas5.givemedetails.view.di.modules.model.ModelTestModule;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
@@ -51,16 +59,52 @@ public class DetailInstrumentedTest {
     }
 
     @Test
-    public void whenActivityLaunched_loadPoster_appearPoster() {
+    public void whenLoadPoster_appearPoster_success() {
         String movieTitleExpected = mContext.getString(R.string.movie_poster_description, mMovie.getTitle());
-        onView(withId(R.id.img_movie_poster))
+        onData(withId(R.id.img_movie_poster))
                 .check(matches(withContentDescription(movieTitleExpected)));
     }
 
     @Test
-    public void whenActivityLaunched_loadTitle_appearTitle() {
+    public void whenLoadSummary_appearTitle_success() {
         String title = mMovie.getTitle();
-        onView(withId(R.id.txt_title)).check(matches(withText(title)));
+        onView(withId(R.id.txt_title)).check(getTextMatcher(title));
+    }
+
+    @Test
+    public void whenLoadSummary_appearOriginalTitle_success() {
+        String originalTitle = mContext.getString(R.string.original_title, mMovie.getOriginalTitle());
+        onData(withId(R.id.txt_original_title))
+                .check(getTextMatcher(originalTitle));
+    }
+
+    @Test
+    public void whenLoadSummary_appearOverview_success() {
+        String overview = mMovie.getOverview();
+        onData(withId(R.id.txt_overview)).check(getTextMatcher(overview));
+    }
+
+    @Test
+    public void whenLoadSummary_appearVoteAverage_success() {
+        String voteAverageAsString = Double.toString(mMovie.getVoteAverage());
+        onData(withId(R.id.txt_vote_average)).check(getTextMatcher(voteAverageAsString));
+    }
+
+    @Test
+    public void whenLoadSummary_appearReleaseDate_success() {
+        String pattern = mContext.getString(R.string.date_format_pattern);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern, Locale.getDefault());
+
+        Date releaseDateAsDate = mMovie.getReleaseDateAsDate();
+        String releaseDateFormatted = mContext.getString(R.string.release_date, simpleDateFormat.format(releaseDateAsDate));
+
+        onData(withId(R.id.txt_release_date))
+                .check(getTextMatcher(releaseDateFormatted));
+    }
+
+    @NonNull
+    private ViewAssertion getTextMatcher(String text) {
+        return matches(withText(text));
     }
 
 }
